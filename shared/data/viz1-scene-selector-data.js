aee7a52e4f95bfd6dbd59b9d9e9e48dab1d0812f
stdanyaa <st.danil.uni@gmail.com>
2026-03-12 21:54:11 +0300

    cleaner descriptions

--- shared/data/viz1-scene-selector-data.js
+++ shared/data/viz1-scene-selector-data.js
@@ -5,46 +5,40 @@ window.XSIM_VIZ1_SCENE_SELECTOR_DATA = {
       label: "Waymo",
       scenes: [
         {
-          id: "016",
-          label: "Scene 016",
-          thumbnail: "static/images/viz1/waymo/016_front.jpg",
-          sceneUrl: "static/videos/viz1/waymo/016_composite.mp4"
-        },
-        {
           id: "021",
           label: "Scene 021",
           thumbnail: "static/images/viz1/waymo/021_front.jpg",
-          sceneUrl: "static/videos/viz1/waymo/021_composite.mp4"
+          sceneUrl: "static/videos/novel_videos/21_frame50_full_nobg/two_row.mp4"
         },
         {
           id: "094",
           label: "Scene 094",
           thumbnail: "static/images/viz1/waymo/094_front.jpg",
-          sceneUrl: "static/videos/viz1/waymo/094_circle.mp4"
+          sceneUrl: "static/videos/novel_videos/94_frame20_circle_up/two_row.mp4"
         },
         {
           id: "344",
           label: "Scene 344",
           thumbnail: "static/images/viz1/waymo/344_front.jpg",
-          sceneUrl: "static/videos/viz1/waymo/344_circle.mp4"
+          sceneUrl: "static/videos/novel_videos/344_frame60_circle_up/two_row.mp4"
         },
         {
           id: "402",
           label: "Scene 402",
           thumbnail: "static/images/viz1/waymo/402_front.jpg",
-          sceneUrl: "static/videos/viz1/waymo/402_circle.mp4"
+          sceneUrl: "static/videos/novel_videos/402_frame110_circle_up/two_row.mp4"
         },
         {
           id: "552",
           label: "Scene 552",
           thumbnail: "static/images/viz1/waymo/552_front.jpg",
-          sceneUrl: "static/videos/viz1/waymo/552_circle.mp4"
+          sceneUrl: "static/videos/novel_videos/552_frame50_circle_up/two_row.mp4"
         },
         {
           id: "555",
           label: "Scene 555",
           thumbnail: "static/images/viz1/waymo/555_front.jpg",
-          sceneUrl: "static/videos/viz1/waymo/555_circle.mp4"
+          sceneUrl: "static/videos/novel_videos/555_frame50_circle/two_row.mp4"
         }
       ]
     }
